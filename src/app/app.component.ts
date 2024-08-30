6a7190f975e9058b5976cd4b4ab1a986b0d3ba5e
Semih Altuntas <altuntas.semih34@gmail.com>
2024-08-30 15:55:29 +0200

    Lesson 3 - Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Altuntas';
+  items= [
+    { description: "Kahvalti", action: "No"},
+    { description: "Sinema", action: "No"},
+    { description: "Spor", action: "No"},
+    { description: "Ders Calisma", action: "No"}
+  ]
 }
