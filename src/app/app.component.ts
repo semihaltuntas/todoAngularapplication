2463278dd483b552848eef1355d904026a74de65
Semih Altuntas <altuntas.semih34@gmail.com>
2024-09-02 11:28:14 +0200

    lastones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Model } from './model';
+import { Model,TodoItem } from './model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
 model = new Model();
+isDisplay=false;
 
 getName(){
   return this.model.user;
 }
 getItems(){
-  return this.model.items;
+  if(this.isDisplay){
+    return this.model.items;
+  }
+  return this.model.items.filter(item=> !item.action);
+}
+addItem(value: string){
+  if(value !== ""){
+    this.model.items.push(new TodoItem(value,false));
+  }
 }
 }
